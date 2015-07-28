f57bed196bdbad029fc5354b511c1bc0f65a2f27
Kailash Shrestha <kshrestha@ainq.com>
2015-07-28 16:15:28 -0400

    Initiate commit

---
+++ src/com/hello/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.hello.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this line is added second time.");
+		System.out.println("Hello to the World.");
+	}
+
+}
